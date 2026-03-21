0e56812ac000abfa87c7a1487906c5274934a4c9
Dmitry Matushkin <dmitry@MacBook-Air-5.local>
2026-03-21 14:18:50 +0300

    Change buffer structure

--- tasks/hw4/mpsc_buffer_ring_queue.h
+++ tasks/hw4/mpsc_buffer_ring_queue.h
@@ -19,17 +19,10 @@
 #include "../../include/utils/error.hpp"
 
 constexpr int PROTOCOL_VERSION = 1;
-constexpr size_t MAX_PAYLOAD_SIZE = 256;
 
 struct MsgHeader {
     int type{};
     int length{};
-};
-
-struct Element {
-    std::atomic<bool> ready{false};
-    MsgHeader header{};
-    char payload[MAX_PAYLOAD_SIZE]{};
 };
 
 struct QueueMeta {
@@ -37,12 +30,29 @@ struct QueueMeta {
     int capacity{};
     std::atomic<int> head_idx{0};
     std::atomic<int> tail_idx{0};
+    std::atomic<int> used_idx{0};
 };
+
+inline void write_ring(std::byte* buffer, int capacity, int offset, const std::byte* data, int size) {
+    int first_part = std::min(size, capacity - (offset % capacity));
+    std::memcpy(buffer + (offset % capacity), data, first_part);
+    if (first_part < size) {
+        std::memcpy(buffer, data + first_part, size - first_part);
+    }
+}
+
+inline void read_ring(const std::byte* buffer, int capacity, int offset, std::byte* output, int size) {
+    int first_part = std::min(size, capacity - (offset % capacity));
+    std::memcpy(output, buffer + (offset % capacity), first_part);
+    if (first_part < size) {
+        std::memcpy(output + first_part, buffer, size - first_part);
+    }
+}
 
 class ProducerNode {
 public:
     ProducerNode(const std::string& shared_file, int capacity) : queue_capacity(capacity) {
-        size_t total_size = sizeof(QueueMeta) + capacity * sizeof(Element);
+        size_t total_size = sizeof(QueueMeta) + capacity;
 
         int fd = shm_open(shared_file.c_str(), O_CREAT | O_RDWR, 0666);
         CHECK_ERROR(fd);
@@ -56,38 +66,46 @@ public:
         queue_meta = new (ptr) QueueMeta{};
         queue_meta->version = PROTOCOL_VERSION;
         queue_meta->capacity = capacity;
-        queue_elements = reinterpret_cast<Element*>(queue_meta + 1);
-        for (int i = 0; i < capacity; ++i) {
-            new (&queue_elements[i]) Element{};
-        }
+        queue_buffer = reinterpret_cast<std::byte*>(queue_meta + 1);
     }
 
     bool push(int type, std::span<const std::byte> msg) {
-        if (msg.size() > MAX_PAYLOAD_SIZE) 
-            return false;
+        int msg_size = msg.size();
+        int required_space = sizeof(MsgHeader) + msg_size;
 
+        if (required_space > queue_capacity) {
+            return false;
+        }
+
+        int head;
         while (true) {
-            auto head = queue_meta->head_idx.load();
+            head = queue_meta->head_idx.load();
             auto tail = queue_meta->tail_idx.load();
 
-            if (head - tail >= queue_capacity) {
+            if (head + required_space - tail > queue_capacity) {
                 return false;
             }
 
-            if (queue_meta->head_idx.compare_exchange_weak(head, head + 1)) {
-                Element& element = queue_elements[head % queue_capacity];
-                element.header.type = type;
-                element.header.length = msg.size();
-                std::memcpy(element.payload, msg.data(), msg.size());
-                element.ready.store(true);
-                return true;
+            if (queue_meta->head_idx.compare_exchange_weak( head, head + required_space)) {
+                break;
             }
         }
+
+        MsgHeader header{type, msg_size};
+        write_ring(queue_buffer, queue_capacity, head, reinterpret_cast<const std::byte*>(&header), sizeof(MsgHeader));
+        write_ring(queue_buffer, queue_capacity, head + sizeof(MsgHeader), msg.data(), msg_size);
+
+        int used = head;
+        while (!queue_meta->used_idx.compare_exchange_weak(used, head + required_space)) {
+            used = head;
+        }
+
+        return true;
     }
 
 private:
     QueueMeta* queue_meta{};
-    Element* queue_elements{};
+    std::byte* queue_buffer{};
     int queue_capacity{};
 };
 
@@ -106,7 +124,7 @@ public:
             throw std::runtime_error("Protocol version mismatch!");
         }
         queue_capacity = init_meta->capacity;
-        size_t total_size = sizeof(QueueMeta) + queue_capacity * sizeof(Element);
+        size_t total_size = sizeof(QueueMeta) + queue_capacity;
 
         void* ptr = mmap(nullptr, total_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         if (ptr == MAP_FAILED) {
@@ -115,7 +133,7 @@ public:
         close(fd);
 
         queue_meta = static_cast<QueueMeta*>(ptr);
-        queue_elements = reinterpret_cast<Element*>(queue_meta + 1);
+        queue_buffer = reinterpret_cast<std::byte*>(queue_meta + 1);
     }
 
     std::optional<std::vector<std::byte>> pop(int type) {
@@ -123,23 +141,25 @@ public:
             std::optional<std::vector<std::byte>> result;
 
             auto tail = queue_meta->tail_idx.load();
-            auto head = queue_meta->head_idx.load();
-            if (tail == head) {
+            auto used = queue_meta->used_idx.load();
+
+            if (tail == used) {
                 return result;
             }
 
-            Element& element = queue_elements[tail % queue_capacity];
-            if (!element.ready.load()) {
+            MsgHeader header;
+            read_ring(queue_buffer, queue_capacity, tail, reinterpret_cast<std::byte*>(&header), sizeof(MsgHeader));
+            int total_msg_size = sizeof(MsgHeader) + header.length;
+            if (tail + total_msg_size > used) {
                 return result;
             }
 
-            bool is_correct_type = (element.header.type == type);
-            element.ready.store(false);
-            queue_meta->tail_idx.store(tail + 1);
+            bool is_correct_type = (header.type == type);
+            queue_meta->tail_idx.store(tail + total_msg_size);
 
             if (is_correct_type) {
-                result = std::vector<std::byte>(element.header.length);
-                std::memcpy(result->data(), element.payload, element.header.length);
+                result = std::vector<std::byte>(header.length);
+                read_ring(queue_buffer, queue_capacity, tail + sizeof(MsgHeader), result->data(), header.length);
                 return result;
             }
         }
@@ -147,6 +167,6 @@ public:
 
 private:
     QueueMeta* queue_meta{};
-    Element* queue_elements{};
+    std::byte* queue_buffer{};
     int queue_capacity{};
 };
--- tasks/hw4/producer.cpp
+++ tasks/hw4/producer.cpp
@@ -2,37 +2,32 @@
 #include <thread>
 #include <chrono>
 
+static std::span<const std::byte> ToBytes(const std::string& s) {
+    return {
+        reinterpret_cast<const std::byte*>(s.data()),
+        s.size()
+    };
+}
+
 int main() {
     static constexpr auto shared_file = "/tmp/mpsc_buffer_ring_queue";
     shm_unlink(shared_file);
 
     try {
-        ProducerNode producer(shared_file, 10);
+        ProducerNode producer(shared_file, 1024);
 
         std::string msg1 = "First msg";
-        std::span<const std::byte> msg_bytes1{
-            reinterpret_cast<const std::byte*>(msg1.data()),
-            msg1.size()
-        };
-        if (producer.push(1, msg_bytes1)) {
+        if (producer.push(1, ToBytes(msg1))) {
             std::cout << std::format("[PRODUCER]: Push №1: {}", msg1) << std::endl;
         }
 
         std::string msg2 = "Second msg";
-        std::span<const std::byte> msg_bytes2{
-            reinterpret_cast<const std::byte*>(msg2.data()),
-            msg2.size()
-        };
-        if (producer.push(2, msg_bytes2)) {
+        if (producer.push(2, ToBytes(msg2))) {
             std::cout << std::format("[PRODUCER]: Push №2: {}", msg2) << std::endl;
         }
 
         std::string msg3 = "Third msg";
-        std::span<const std::byte> msg_bytes3{
-            reinterpret_cast<const std::byte*>(msg3.data()),
-            msg3.size()
-        };
-        if (producer.push(1, msg_bytes3)) {
+        if (producer.push(1, ToBytes(msg3))) {
             std::cout << std::format("[PRODUCER]: Push №3: {}", msg3) << std::endl;
         }
 
